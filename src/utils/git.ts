446c85ba3f7190b41abcd82716808ca7b3219ffa
Rafal Piekara <rafal@everai.ai>
2025-07-23 15:07:04 +0200

    feat: implement git utilities

---
+++ src/utils/git.ts
@@ -0,0 +1,20 @@
+import simpleGit, { SimpleGit } from 'simple-git';
+
+const git: SimpleGit = simpleGit();
+
+interface ModifiedFiles {
+    committed: string[];
+    uncommitted: string[];
+}
+
+export const getModifiedFiles = async (mainBranch: string = 'main'): Promise<ModifiedFiles> => {
+    const committed = (await git.diff(['--name-only', `${mainBranch}...HEAD`])).split('\n').filter(Boolean);
+
+    const status = await git.status();
+    const uncommitted = status.files.map(file => file.path);
+
+    return {
+        committed,
+        uncommitted
+    };
+};
